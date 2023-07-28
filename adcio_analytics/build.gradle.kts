a23b7bf136afbf3f23ac9705a3016f58a42c50ec
yoohyunmyeong <aahspringaa4@naver.com>
2023-07-28 12:48:09 +0900

    chore: implement jitpack distribution gradle setting

--- adcio_analytics/build.gradle.kts
+++ adcio_analytics/build.gradle.kts
@@ -1,3 +1,4 @@
+
 plugins {
     id("com.android.library")
     id("org.jetbrains.kotlin.android")
@@ -45,3 +46,5 @@ dependencies {
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
 }
+
+group=("com.github.aahspringaa4")
