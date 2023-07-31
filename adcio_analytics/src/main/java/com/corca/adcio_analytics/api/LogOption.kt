48ca9b72736cc87cb037b7ffc2693b89eb904c98
yoohyunmyeong <aahspringaa4@naver.com>
2023-07-31 10:09:33 +0900

    perf: reduce naming dependency by serializedname

--- adcio_analytics/src/main/java/com/corca/adcio_analytics/api/LogOption.kt
+++ adcio_analytics/src/main/java/com/corca/adcio_analytics/api/LogOption.kt
@@ -1,12 +1,14 @@
 package com.corca.adcio_analytics.api
 
+import com.google.gson.annotations.SerializedName
+
 data class LogOption(
-    val requestId: String = "",
-    val cost: Int = 0,
-    val sessionId: String = "",
-    val memberId: String = "",
-    val campaignId: String = "",
-    val productId: String = "",
-    val price: Int = 0,
-    val fromAgent: Boolean = false
+    @field:SerializedName("requestId") val requestId: String = "",
+    @field:SerializedName("cost") val cost: Int = 0,
+    @field:SerializedName("sessionId") val sessionId: String = "",
+    @field:SerializedName("memberId")  val memberId: String = "",
+    @field:SerializedName("campaignId") val campaignId: String = "",
+    @field:SerializedName("productId") val productId: String = "",
+    @field:SerializedName("price") val price: Int = 0,
+    @field:SerializedName("fromAgent") val fromAgent: Boolean = false,
 )
