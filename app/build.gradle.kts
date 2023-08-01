12eaf389125b8886815c409584f1da379cdf6242
yoohyunmyeong <aahspringaa4@naver.com>
2023-08-01 10:16:23 +0900

    chore: add dotenv dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -38,6 +38,7 @@ android {
 dependencies {
 
     implementation(project(":adcio_analytics"))
+    implementation("io.github.cdimascio:dotenv-kotlin:6.4.1")
     implementation("androidx.core:core-ktx:1.9.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.8.0")
