70c3c5bedbab71dcd2235b3132c1466db470d5d9
yoohyunmyeong <aahspringaa4@naver.com>
2023-08-01 10:17:05 +0900

    style: change sessionid to optional

--- adcio_analytics/src/main/java/com/corca/adcio_analytics/api/LogOption.kt
+++ adcio_analytics/src/main/java/com/corca/adcio_analytics/api/LogOption.kt
@@ -1,14 +1,15 @@
 package com.corca.adcio_analytics.api
 
+import android.os.Build
 import com.google.gson.annotations.SerializedName
 
 data class LogOption(
-    @field:SerializedName("requestId") val requestId: String = "",
-    @field:SerializedName("cost") val cost: Int = 0,
-    @field:SerializedName("sessionId") val sessionId: String = "",
-    @field:SerializedName("memberId")  val memberId: String = "",
-    @field:SerializedName("campaignId") val campaignId: String = "",
-    @field:SerializedName("productId") val productId: String = "",
-    @field:SerializedName("price") val price: Int = 0,
-    @field:SerializedName("fromAgent") val fromAgent: Boolean = false,
+    @field:SerializedName("requestId") val requestId: String,
+    @field:SerializedName("cost") val cost: Int,
+    @field:SerializedName("sessionId") val sessionId: String = Build.ID,
+    @field:SerializedName("memberId")  val memberId: String,
+    @field:SerializedName("campaignId") val campaignId: String,
+    @field:SerializedName("productId") val productId: String,
+    @field:SerializedName("price") val price: Int,
+    @field:SerializedName("fromAgent") val fromAgent: Boolean,
 )
